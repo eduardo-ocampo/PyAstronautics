ac32695ae5fd5c639f4be7fc205b876238ca4639
Eduardo Ocampo <eocampo3@gmail.com>
2024-10-13 23:43:57 -0700

    1 setup project (#2)
    * Setup pyproject and include docs/src templates
    
    * remove unwanted ds files

---
+++ docs/source/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyAstronautics'
+copyright = '2024, Eduardo Ocampo'
+author = 'Eduardo Ocampo'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_wagtail_theme",
+    "myst_parser"
+]
+
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+    "deflist",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+    # "linkify",
+    # "strikethrough",
+    "substitution",
+    "tasklist",
+]
+myst_heading_anchors = 4
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = "sphinx_wagtail_theme"
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
+html_theme_options = {
+       "project_name": project,
+    #    "default_mode": "light"
+}
---
+++ docs/source/index.md
@@ -0,0 +1,18 @@
+
+Welcome to PyAstronautics' Documentation!
+============================================
+
+This package is a collection of Python modules I wrote to support my master's level studies in Astronautical Engineering at the University of Southern California. The long form documentation is for the curious readers who continue to seek knowledge and explore new ideas. Enjoy!
+
+This work is dedicated to Mae🌻 and Moose🐶. Your love and joy inspire me every day. Thank you for being my greatest supporters.
+
+\- Eduardo Ocampo
+
+---
+
+## Table of Contents
+
+```{toctree}
+:maxdepth: 4
+astrodynamics/index.md
+```
