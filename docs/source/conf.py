294f08c38f661854b57aa3597a1f3454fc17119f
Eduardo Ocampo <eocampo3@gmail.com>
2024-10-19 22:46:50 -0700

    Resolve pypi package deployment (#10)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'PyAstronautics'
 copyright = '2024, Eduardo Ocampo'
 author = 'Eduardo Ocampo'
-release = '0.0.1'
+release = '0.0.15'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
